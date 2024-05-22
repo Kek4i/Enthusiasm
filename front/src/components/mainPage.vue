3896c13ee834eee89d4904853b84bbc359f28333
coold <cool.denis12332@gmail.com>
2024-05-22 19:46:56 +0300

    Создан слайдер в mainPage.vue
    ____________________________________________________
    
    Created a slider in mainPage.vue

--- front/src/components/mainPage.vue
+++ front/src/components/mainPage.vue
@@ -9,12 +9,20 @@
                 :key="index"
                 :class="['slide', { active: index === currentSlide }]"
             >
-              <h2>{{ slide.title }}</h2>
-              <p>{{ slide.description }}</p>
-              <img :src="slide.image" :alt="slide.title" />
-              <div>
-                <button>Играть</button>
-                <button>Подробнее</button>
+              <div class="page__container__slider__slideContainer__info">
+                <div class="page__container__slider__slideContainer__title">{{ slide.title }}</div>
+                <div class="page__container__slider__slideContainer__description">{{ slide.description }}</div>
+                <div class="page__container__slider__slideContainer__buttons">
+                  <router-link to="/play" class="custom-button">
+                    Играть
+                  </router-link>
+                  <router-link to="/servers" class="custom-button">
+                    Подробнее
+                  </router-link>
+                </div>
+              </div>
+              <div class="page__container__slider__slideContainer__image">
+                <img :src="slide.image" :alt="slide.title" />
               </div>
             </div>
           </div>
@@ -34,10 +42,10 @@
 
 <style scoped>
 .main_content {
+  max-width: 1320px;
   margin-top: 32px;
   margin-inline: auto;
   display: flex;
-  justify-content: space-between;
   width: 100%;
 }
 
@@ -50,9 +58,7 @@
   width: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
-  gap: 128px;
 }
 
 .page__container__slider {
@@ -63,42 +69,107 @@
 
 .page__container__slider__slideContainer {
   display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
   overflow: hidden;
   width: 100%;
   position: relative;
 }
 
 .slide {
-  min-width: 100%;
-  transition: transform 0.5s ease-in-out;
-  opacity: 0;
-  position: absolute;
-  top: 0;
-  left: 0;
+  display: none;
+  align-items: center;
 }
 
 .slide.active {
-  opacity: 1;
-  position: relative;
+  display: flex;
+}
+
+.page__container__slider__slideContainer__info {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  max-width: 50%;
+  text-align: left;
+  align-items: flex-start;
+}
+
+.page__container__slider__slideContainer__image {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 50%;
+  margin-left: 50px;
+}
+
+.page__container__slider__slideContainer__image img {
+  width: 660px;
+  height: 373px;
+  object-fit: cover;
+  border-radius: 6px;
+}
+
+.page__container__slider__slideContainer__title {
+  color: #fff;
+  font-size: 64px;
+  font-weight: 800;
+  text-transform: uppercase;
+}
+
+.page__container__slider__slideContainer__description {
+  color: #fff;
+  font-size: 24px;
+}
+
+.page__container__slider__slideContainer__buttons {
+  display: flex;
+  gap: 24px;
 }
 
 .page__container__slider__slideNav {
   display: flex;
-  justify-content: center;
-  gap: 10px;
+  padding: 32px 10px;
+  align-items: flex-start;
+  gap: 24px;
 }
 
 .page__container__slider__slideNav button {
-  width: 10px;
-  height: 10px;
-  border-radius: 50%;
-  border: none;
-  background-color: gray;
+  width: 70px;
+  height: 16px;
+  border-radius: 8px;
   cursor: pointer;
+  background: rgba(255, 255, 255, 0.05);
+  border: initial;
 }
 
 .page__container__slider__slideNav button.active {
   background: linear-gradient(132deg, rgb(255, 95, 109) 0%, rgb(255, 195, 113) 100%);
+}
+
+.custom-button {
+  display: flex;
+  padding: 16px 20px;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  border-radius: 8px;
+  background: #ff9370;
+  color: #fff;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  text-transform: uppercase;
+  text-decoration: none;
+}
+
+.custom-button:hover {
+  background: #ff8261; /* Optionally add hover effects */
+}
+
+.custom-button:active {
+  background: #ff7152; /* Optionally add active effects */
 }
 </style>
 
@@ -137,10 +208,16 @@ let intervalId;
 
 function setSlide(index) {
   currentSlide.value = index;
+  resetTimer();
 }
 
 function nextSlide() {
   currentSlide.value = (currentSlide.value + 1) % slides.value.length;
+}
+
+function resetTimer() {
+  clearInterval(intervalId);
+  intervalId = setInterval(nextSlide, 10000);
 }
 
 onMounted(() => {
